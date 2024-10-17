4dfc5ee88a23c3d9d947f04298afafca03174c14
winechit-dev <winechitpaing.dev@gmail.com>
2024-10-17 14:18:33 +0700

    - update button design

--- core/designsystem/src/main/java/com/moviequest/designsystem/components/Button.kt
+++ core/designsystem/src/main/java/com/moviequest/designsystem/components/Button.kt
@@ -1,22 +1,26 @@
 package com.moviequest.designsystem.components
 
 import androidx.annotation.DrawableRes
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.defaultMinSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.shape.CornerSize
-import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonColors
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.ProvideTextStyle
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
@@ -25,6 +29,7 @@ import androidx.compose.ui.tooling.preview.PreviewParameterProvider
 import androidx.compose.ui.unit.dp
 import com.moviequest.designsystem.R
 import com.moviequest.designsystem.theme.AppPreview
+import com.moviequest.designsystem.theme.asBrush
 import com.moviequest.designsystem.theme.defaultButton
 
 val MinHeight = 43.dp
@@ -38,11 +43,26 @@ fun AppButton(
     textStyle: TextStyle = MaterialTheme.typography.defaultButton,
     onClick: () -> Unit
 ) {
+
+    val disabledContainerColor = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.12f).asBrush()
+    val containerColor = Brush.horizontalGradient(
+        colors = listOf(
+            MaterialTheme.colorScheme.secondary,
+            MaterialTheme.colorScheme.primary
+        )
+    )
+    val brush = remember(enabled) {
+        if (enabled) containerColor else disabledContainerColor
+    }
+
     Button(
         onClick = onClick,
-        modifier = modifier.defaultMinSize(minHeight = MinHeight),
+        modifier = modifier
+            .background(brush = brush, shape = CircleShape)
+            .defaultMinSize(minHeight = MinHeight),
         enabled = enabled,
-        shape = defaultButtonShape,
+        shape = CircleShape,
+        colors = defaultButtonColors(),
         content = {
             ProvideTextStyle(value = textStyle) {
                 Row(
@@ -61,15 +81,11 @@ fun AppButton(
     )
 }
 
-
-val defaultButtonShape: Shape
-    get() = RoundedCornerShape(
-        topStart = CornerSize(50),
-        topEnd = CornerSize(50),
-        bottomStart = CornerSize(50),
-        bottomEnd = CornerSize(50)
-    )
-
+@Composable
+fun defaultButtonColors(): ButtonColors = ButtonDefaults.buttonColors(
+    containerColor = Color.Transparent,
+    disabledContainerColor = Color.Transparent
+)
 
 class ButtonPreviewParameterProvider : PreviewParameterProvider<Boolean> {
     override val values: Sequence<Boolean>
