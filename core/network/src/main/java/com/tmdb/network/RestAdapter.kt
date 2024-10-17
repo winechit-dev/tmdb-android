eb3a9adc8eeb2de8895f90093880f8a873213673
winechit-dev <winechitpaing.dev@gmail.com>
2024-10-17 21:37:46 +0700

    - create rest adapter

---
+++ core/network/src/main/java/com/tmdb/network/RestAdapter.kt
@@ -0,0 +1,43 @@
+package com.tmdb.network
+
+import android.content.Context
+import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
+import java.util.concurrent.TimeUnit
+import kotlinx.serialization.json.Json
+import okhttp3.MediaType.Companion.toMediaType
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+
+inline fun <reified T> Retrofit.createService(): T = create(T::class.java)
+
+fun createRetrofitClient(
+    url: String,
+    okHttpClient: OkHttpClient,
+    networkJson: Json
+): Retrofit = Retrofit.Builder()
+    .baseUrl(url)
+    .client(okHttpClient)
+    .addConverterFactory(NullOnEmptyConverterFactory())
+    .addConverterFactory(
+        networkJson.asConverterFactory("application/json".toMediaType())
+    ).build()
+
+
+fun createOkHttpClient(
+    context: Context,
+    httpLoggingInterceptor: HttpLoggingInterceptor,
+) = OkHttpClient.Builder()
+    .also {
+        if (BuildConfig.DEBUG) {
+            it.addInterceptor(
+                httpLoggingInterceptor.apply {
+                    level = HttpLoggingInterceptor.Level.BODY
+                }
+            )
+        }
+    }
+    .connectTimeout(60L, TimeUnit.SECONDS)
+    .readTimeout(60L, TimeUnit.SECONDS)
+    .writeTimeout(60L, TimeUnit.SECONDS)
+    .build()
