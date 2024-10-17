87693d197e14d3be1f527a2a4855b6e0adc7049a
winechit-dev <winechitpaing.dev@gmail.com>
2024-10-17 21:38:17 +0700

    - create network module

---
+++ core/network/src/main/java/com/tmdb/network/di/NetworkModule.kt
@@ -0,0 +1,59 @@
+package com.tmdb.network.di
+
+import android.content.Context
+import com.tmdb.network.BuildConfig
+import com.tmdb.network.createOkHttpClient
+import com.tmdb.network.createRetrofitClient
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.SupervisorJob
+import kotlinx.serialization.json.Json
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+
+@Module
+@InstallIn(SingletonComponent::class)
+abstract class NetworkModule {
+
+    @Module
+    @InstallIn(SingletonComponent::class)
+    object Provider {
+        @Provides
+        @Singleton
+        fun providesNetworkJson(): Json = Json {
+            ignoreUnknownKeys = true
+            explicitNulls = false
+        }
+
+        @Singleton
+        @Provides
+        fun providesCoroutineScope(): CoroutineScope {
+            return CoroutineScope(SupervisorJob() + Dispatchers.Default)
+        }
+
+        @Provides
+        @Singleton
+        fun provideOkHttpClient(
+            @ApplicationContext
+            context: Context
+        ): OkHttpClient {
+            return createOkHttpClient(
+                context,
+                HttpLoggingInterceptor()
+            )
+        }
+
+        @Provides
+        @Singleton
+        fun provideRetrofit(okHttpClient: OkHttpClient, networkJson: Json): Retrofit {
+            return createRetrofitClient(BuildConfig.BASE_URL, okHttpClient, networkJson)
+        }
+    }
+}
