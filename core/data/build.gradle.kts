c473f5424c402fe35e9bd376f016939b1ee5633c
winechit-dev <winechitpaing.dev@gmail.com>
2024-10-17 21:38:32 +0700

    - add dependencies

--- core/data/build.gradle.kts
+++ core/data/build.gradle.kts
@@ -1,7 +1,15 @@
 plugins {
     alias(libs.plugins.convention.library)
+    id("kotlinx-serialization")
 }
 
 android {
     namespace = "com.tmdb.data"
+}
+
+dependencies {
+    api(libs.retrofit.core)
+    api(libs.okhttp.logging)
+    implementation(libs.kotlinx.serialization.json)
+    implementation(libs.retrofit.kotlin.serialization)
 }
--- core/network/build.gradle.kts
+++ core/network/build.gradle.kts
@@ -1,7 +1,21 @@
+
 plugins {
     alias(libs.plugins.convention.library)
 }
 
 android {
     namespace = "com.tmdb.network"
+
+    defaultConfig {
+        buildConfigField ("String", "BASE_URL", "\"${project.extra["BASE_URL"] as String}\"")
+        buildConfigField ("String", "API_KEY", "\"${project.extra["API_KEY"] as String}\"")
+    }
+}
+
+dependencies {
+    implementation(libs.kotlinx.serialization.json)
+    implementation(libs.retrofit.core)
+    implementation(libs.retrofit.kotlin.serialization)
+    implementation(libs.okhttp.logging)
+    implementation(libs.arrow)
 }
