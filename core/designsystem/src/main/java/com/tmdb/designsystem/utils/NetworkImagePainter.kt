858217f4256da6f2a3eebc4bea973b5f13ac2118
winechit-dev <winechitpaing.dev@gmail.com>
2024-10-18 15:43:57 +0700

    - add error placeholder

--- core/designsystem/src/main/java/com/tmdb/designsystem/utils/NetworkImagePainter.kt
+++ core/designsystem/src/main/java/com/tmdb/designsystem/utils/NetworkImagePainter.kt
@@ -5,6 +5,7 @@ import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.platform.LocalContext
 import coil.compose.rememberAsyncImagePainter
 import coil.request.ImageRequest
+import com.tmdb.designsystem.R
 
 @Composable
 fun networkImagePainter(
@@ -13,6 +14,7 @@ fun networkImagePainter(
     model = ImageRequest.Builder(LocalContext.current)
         .data(url)
         .crossfade(true)
+        .error(R.drawable.ic_placeholder_default)
         .size(coil.size.Size.ORIGINAL)
         .build(),
 )
